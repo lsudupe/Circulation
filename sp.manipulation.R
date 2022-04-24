77d8298639b7c4c4874fb55fe5174340ce6d0c88
lsudupe <sudupelaura@gmail.com>
2022-04-24 10:20:43 +0300

    take out sample names and change fb cluster annotatin

--- sp.manipulation.R
+++ sp.manipulation.R
@@ -12,18 +12,18 @@ library("ggplot2")
 library("data.table")
 
 ##Read the data
-spatial <- readRDS("./objects/integrated/integrated.gfp.rds")
+spatial <- readRDS("./objects/integrated/integrated.sct.rds")
 
 ####SUBSET THE SPATIAL DATA
 
 spatial@meta.data[["ident"]] <- spatial@meta.data[["seurat_clusters"]]
 Seurat::Idents(object = spatial) <- spatial@meta.data[["ident"]]
 
-spatial.fb <- subset(x = spatial, idents = c("1","4","5","6","8"))
-spatial.no.fb <- subset(x = spatial, idents = c("0","2","3","7"))
+spatial.fb <- subset(x = spatial, idents = c("1","2","3","5"))
+spatial.no.fb <- subset(x = spatial, idents = c("0","4","6"))
 
 spatial.fb@meta.data[["ident"]] <- spatial.fb@active.ident
 spatial.no.fb@meta.data[["ident"]] <- spatial.no.fb@active.ident
 
-saveRDS(spatial.fb, file = "./objects/integrated/integrated.gfp.fb.rds")
-saveRDS(spatial.no.fb, file = "./objects/integrated/integrated.gfp.nofb.rds")
+saveRDS(spatial.fb, file = "./objects/integrated/integrated.fb.rds")
+saveRDS(spatial.no.fb, file = "./objects/integrated/integrated.nofb.rds")
