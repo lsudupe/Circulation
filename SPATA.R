3f7f0840cef497bc1d80c5fc92a8b85bc9bb515f
lsudupe <sudupelaura@gmail.com>
2022-05-16 13:51:15 +0300

    add all object transformation function and save them

--- SPATA.R
+++ SPATA.R
@@ -59,11 +59,44 @@ dpi3_spata <- transformSeuratToSpata(
   verbose = TRUE
 )
 
-dpi3_spata <- SPATA2::createSegmentation(dpi3_spata)
-SPATA2::plotSegmentation(object = dpi3_spata)
-plotSurface(object = dpi3_spata, color_by = "segmentation")
+dpi5_male_spata <- transformSeuratToSpata(
+  dpi5_male,
+  sample_name = "dpi5_male",
+  method = "spatial",
+  coords_from = "pca",
+  assay_name = "SCT",
+  assay_slot = NULL,
+  image_name = "dpi5_male",
+  gene_set_path = NULL,
+  verbose = TRUE
+)
 
 #https://rdrr.io/github/kueckelj/SPATA2/man/transformSeuratToSpata.html
+
+dpi5_female_spata <- transformSeuratToSpata(
+  dpi5_female,
+  sample_name = "dpi5_female",
+  method = "spatial",
+  coords_from = "pca",
+  assay_name = "SCT",
+  assay_slot = NULL,
+  image_name = "dpi5_female",
+  gene_set_path = NULL,
+  verbose = TRUE
+)
+
+saveRDS(dpi3_spata, "./objects/spata/dpi3_spata.rds")
+saveRDS(dpi5_female_spata, "./objects/spata/dpi5_female_spata.rds")
+saveRDS(dpi5_male_spata, "./objects/spata/dpi5_male_spata.rds")
+
+dpi3_spata <- readRDS("./objects/spata/dpi3_spata.rds")
+dpi5_female_spata <- readRDS("./objects/spata/dpi5_female_spata.rds")
+dpi5_male_spata <- readRDS("./objects/spata/dpi5_male_spata.rds")
+
+
+dpi5_male_spata <- SPATA2::createSegmentation(dpi5_male_spata)
+SPATA2::plotSegmentation(object = dpi5_male_spata)
+plotSurface(object = dpi3_spata, color_by = "segmentation")
 
 SPATA2::createSegmentation(dpi3_spata)
 SPATA2::plotSegmentation(object = dpi3_spata)
