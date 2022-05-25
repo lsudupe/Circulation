eeca563c5df11d822eeee2212729f308649037d0
lsudupe <sudupelaura@gmail.com>
2022-05-25 10:48:36 +0300

    script to add segmentation info and plots

---
+++ segmentation.R
@@ -0,0 +1,39 @@
+## SCRIPT: individual analysis AUCell and segmentation spatial samples CIRCULATION project
+
+## 05.04.22 Laura Sudupe , git @lsudupe
+
+#Libraries---------------------------------
+library(Seurat)
+library(dplyr)
+library(ggplot2)
+
+
+#Data---------------------------------
+control <- readRDS("./results/individual/control.rds")
+dpi3 <- readRDS("./results/individual/dpi3.rds")
+dpi5_female <- readRDS("./results/individual/dpi5_female.rds")
+dpi5_male <- readRDS("./results/individual/dpi5_male.rds")
+
+
+enrichment.meta <- read.csv( "./data/infarto_5dpi_hembra/enrichment_2.csv")
+enrichment.meta <- as.vector(enrichment.meta$Enrichment)
+
+dpi5_female@meta.data["segmen"] <- as.factor(enrichment.meta)
+d1d2 <- as.vector(dpi5_female@meta.data[["relation_log.d1.d2"]])
+
+library(robustHD)
+library(data.table)
+d1d2<- standardize(d1d2, centerFun = mean, scaleFun = sd)
+dpi5_female@meta.data["d1d2"] <- d1d2
+
+SpatialPlot(dpi5_female, group.by = c("segmen"),label = TRUE, combine = FALSE)
+
+pdf(file.path("./results/individual/2.prueba",filename = paste("relation_log.d1.d2.boxplot.pdf",sep="")))
+dpi5_female@meta.data%>% 
+  ggplot(aes(x=relation_log.d1.d2, y= segmen, fill=segmen)) + 
+  geom_boxplot() +  
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  #xlim(-2.5, 2) +
+  theme(plot.title = element_text(hjust=0.5, face="bold"))
+dev.off()
