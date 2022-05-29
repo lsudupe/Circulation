c09ac90bc7fb5e1ce8941025cb73ebfcc12c531d
lsudupe <sudupelaura@gmail.com>
2022-05-29 09:37:41 +0300

    save segmentation data, order scripts

--- segmentation.R
+++ segmentation.R
@@ -16,19 +16,55 @@ dpi3 <- readRDS("./results/individual/dpi3.rds")
 dpi5_female <- readRDS("./results/individual/dpi5_female.rds")
 dpi5_male <- readRDS("./results/individual/dpi5_male.rds")
 
-
-
 ######dpi3
-enrichment.meta <- read.csv( "./data/infarto_3dpi/Enrichment.csv")
+enrichment.meta <- read.csv( "./data/dpi3/Enrichment.csv")
 enrichment.meta <- as.vector(enrichment.meta$Enrichment)
 
-dpi3e@meta.data["segmen"] <- as.factor(enrichment.meta)
+dpi3@meta.data["segmen"] <- as.factor(enrichment.meta)
 d1d2<- as.vector(dpi3@meta.data[["relation_log.d1.d2"]])
-
-
 d1d2<- standardize(d1d2, centerFun = mean, scaleFun = sd)
 dpi3@meta.data["d1d2"] <- d1d2
 
+saveRDS(dpi3, "./objects/individual/segmentation/dpi3.seg.rds")
+
+######dpi5_female
+enrichment.meta <- read.csv( "./data/dpi5_female/Enrichment.csv")
+enrichment.meta <- as.vector(enrichment.meta$Enrichment)
+
+dpi5_female@meta.data["segmen"] <- as.factor(enrichment.meta)
+d1d2<- as.vector(dpi5_female@meta.data[["relation_log.d1.d2"]])
+d1d2<- standardize(d1d2, centerFun = mean, scaleFun = sd)
+dpi5_female@meta.data["d1d2"] <- d1d2
+
+saveRDS(dpi5_female, "./objects/individual/segmentation/dpi5_female.seg.rds")
+
+######dpi5_male
+enrichment.meta <- read.csv( "./data/dpi5_male/Enrichment.csv")
+enrichment.meta <- as.vector(enrichment.meta$Enrichment)
+
+dpi5_male@meta.data["segmen"] <- as.factor(enrichment.meta)
+d1d2<- as.vector(dpi5_male@meta.data[["relation_log.d1.d2"]])
+d1d2<- standardize(d1d2, centerFun = mean, scaleFun = sd)
+dpi5_male@meta.data["d1d2"] <- d1d2
+
+saveRDS(dpi5_male, "./objects/individual/segmentation/dpi5_male.seg.rds")
+
+#####################
+
+
+#Create a custom color scale
+library(RColorBrewer)
+myColors <- brewer.pal(7,"Paired")
+#myColors <- as.vector(names(myColors))
+seg <- as.factor(c("IZ","BZ_1","BZ_2","BZ_3","BZ_2_3","BZ_4","RZ"))
+names(myColors) <- levels(seg)
+#colScale <- scale_colour_manual(name = "grp",values = myColors)
+
+nombres <- c("IZ","BZ_1","BZ_2","BZ_3","BZ_2_3","BZ_4","RZ")
+colors <- c("coral4", "skyblue", "red", "darkgreen", "yellow","khaki2", "darkolivegreen", "gray89")
+names(colors)  <- nombres
+
+####################dpi3
 SpatialPlot(dpi3, group.by = c("segmen"),label = TRUE, combine = FALSE)
 
 pdf(file.path("./results/individual/dpi3",filename = paste("d1d2.boxplot.pdf",sep="")))
@@ -41,25 +77,33 @@ dpi3@meta.data%>%
   theme(plot.title = element_text(hjust=0.5, face="bold"))
 dev.off()
 
-######dpi5_male
-enrichment.meta <- read.csv( "./data/infarto_5dpi_macho/Enrichment.csv")
-enrichment.meta <- as.vector(enrichment.meta$Enrichment)
+####################dpi5_female
 
-dpi5_male@meta.data["segmen"] <- as.factor(enrichment.meta)
-d1d2 <- as.vector(dpi5_male@meta.data[["relation_log.d1.d2"]])
+pdf(file.path("./results/individual/dpi5_male",filename = paste("prueba..pdf",sep="")))
+dpi3@meta.data%>% 
+  ggplot(aes(x=geneSetB, y= segmen, fill=segmen)) + 
+  geom_boxplot(aes(fill=segmen)) +  
+  scale_fill_manual(values =colors ) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+  #xlim(-0.5, 0.5) +
+  theme(plot.title = element_text(hjust=0.5, face="bold")) 
+dev.off()
 
+pdf(file.path("./results/individual/dpi5_male",filename = paste("spatial.seg.pdf",sep="")))
+SpatialPlot(dpi5_male, group.by = c("segmen"),label = TRUE, cols = myColors,combine = FALSE)
+dev.off()
 
-d1d2<- standardize(d1d2, centerFun = mean, scaleFun = sd)
-dpi5_male@meta.data["d1d2"] <- d1d2
+####################dpi5_male
 
-SpatialPlot(dpi5_male, group.by = c("segmen"),label = TRUE, combine = FALSE)
-
-pdf(file.path("./results/individual/dpi5_male",filename = paste("d1d2.boxplot.pdf",sep="")))
-dpi5_male@meta.data%>% 
+pdf(file.path("./results/individual/dpi5_male",filename = paste("prueba..pdf",sep="")))
+  dpi5_male@meta.data%>% 
   ggplot(aes(x=d1d2, y= segmen, fill=segmen)) + 
   geom_boxplot() +  
+  scale_color_manual(colScale) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
   xlim(-0.5, 0.5) +
-  theme(plot.title = element_text(hjust=0.5, face="bold"))
+  theme(plot.title = element_text(hjust=0.5, face="bold")) 
 dev.off()
+
