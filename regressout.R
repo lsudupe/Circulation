921f0a25677ea5bbe5f22ec741ccb473a95f3052
lsudupe <sudupelaura@gmail.com>
2022-06-02 11:41:22 +0300

    code to add AUCell enrichment FB score to the objects

--- regressout.R
+++ regressout.R
@@ -36,29 +36,46 @@ write.csv(top.200.de,"./results/DE/top200_tokio_FB.rds", row.names = FALSE)
 fb.genes <- as.vector(top.200.de$gene)
 fb.sig <- GeneSet(fb.genes, setName="geneSetFB")
 
-####AUCell score for each object?
-###matrix
-a <- objects[[i]]
-a@assays[["SCT"]]@counts <- a@assays[["SCT"]]@data
-matrix <- a@assays[["SCT"]]@counts
-matrix <- as.matrix(matrix)
-###### AUC score 
-cells_rankings <- AUCell_buildRankings(matrix, nCores=1)#, plotStats=TRUE)
-#rankings <- getRanking(cells_rankings)
-cells_AUC <- AUCell_calcAUC(geneSets, cells_rankings,aucMaxRank=genes.porcentage)
-#extract AUC values
-auc_per_cell_all <- as.data.frame(t(getAUC(cells_AUC)))
-#calculate relation d1/d2
-d1 <- as.vector(auc_per_cell_all$geneSetD1)
-d2 <- as.vector(auc_per_cell_all$geneSetD2)
-d1.d2 <- log10(d1/d2)
-is.na(d1.d2) <-sapply(d1.d2, is.infinite)
-d1.d2[is.na(d1.d2)] = 0
-auc_per_cell_all$relation_log.d1.d2 <- d1.d2
-##save meta
-a <- AddMetaData(a, auc_per_cell_all)
-saveRDS(a,file = paste0("./results/individual/",names(objects[i]),".rds"))
+###### Read the gene sets
+genes.velocity <- read.csv("./data/genes_velocity.csv", header = TRUE, sep = ",")
+B <- read.table("./data/clustB_signature.txt", header = FALSE)
+B <- as.vector(B$V1)
+D1 <- as.vector(genes.velocity$Dynamics_1)
+D2 <- as.vector(genes.velocity$Dynamics_2)
+D2[D2 == ""] <- NA 
+D2 <- D2[!is.na(D2)]
 
+####Check how many genes are the same in each signature
+FB.B <- b.fibro <- intersect(fb.genes, B)
+FB.D1 <- b.fibro <- intersect(fb.genes, D1)
+FB.D2 <- b.fibro <- intersect(fb.genes, D2)
 
-sctransform::
+##########################################3
+
+control <- readRDS("./results/individual/control.rds")
+dpi3 <- readRDS("./results/individual/dpi3.rds")
+dpi5_female <- readRDS("./results/individual/dpi5_female.rds")
+dpi5_male <- readRDS("./results/individual/dpi5_male.rds")
+
+objects <- c(control, dpi3, dpi5_female, dpi5_male)
+names(objects) <- c("control", "dpi3","dpi5_female","dpi5_male")
+
+###############################################333
+for (i in 1:length(objects)){
+  ###matrix
+  a <- objects[[i]]
+  a@assays[["SCT"]]@counts <- a@assays[["SCT"]]@data
+  matrix <- a@assays[["SCT"]]@counts
+  matrix <- as.matrix(matrix)
+  ###### AUC score 
+  cells_rankings <- AUCell_buildRankings(matrix, nCores=1)#, plotStats=TRUE)
+  #rankings <- getRanking(cells_rankings)
+  cells_AUC <- AUCell_calcAUC(fb.sig, cells_rankings,aucMaxRank=genes.porcentage)
+  #extract AUC values
+  auc_per_cell_all <- as.data.frame(t(getAUC(cells_AUC)))
+  ##save meta
+  a <- AddMetaData(a, auc_per_cell_all)
+  saveRDS(a,file = paste0("./results/individual/",names(objects[i]),"FB.rds"))
+  
+}
 
