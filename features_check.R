76430ae2d326444e9385d81361a9eb029e94a4b7
lsudupe <sudupelaura@gmail.com>
2023-08-07 10:17:12 +0900

    add linear regression with fb influence

--- features_check.R
+++ features_check.R
@@ -8,6 +8,8 @@ library("Seurat")
 library("GSEABase")
 library("ggplot2")
 library(UCell)
+library(RColorBrewer)
+library(dplyr)
 set.seed(123)
 
 ###### Read the gene sets
@@ -15,10 +17,25 @@ genes <- read.xlsx("./Circulation/data/Silvia_clean.xlsx", colNames = TRUE)
 verde <- na.omit(genes$verde)
 morado <- na.omit(genes$morado)
 
+fb.genes <- read.csv("./Circulation/results/DE/top200_tokio_FB.csv")
+fb.g <- na.omit(fb.genes$gene)
+
+nine <- read.csv("./Circulation/results/genes/top100nine.csv")
+nine.g <- na.omit(nine$x)
+
+genes.velocity <- read.csv("./Circulation/data/genes_velocity.csv", header = TRUE, sep = ",")
+B <- read.table("./Circulation/data/clustB_signature.txt", header = FALSE)
+B <- as.vector(B$V1)
+DA <- as.vector(genes.velocity$Dynamics_1)
+DB <- as.vector(genes.velocity$Dynamics_2)
+DB[DB == ""] <- NA 
+DB <- DB[!is.na(DB)]
+
 ###### Read data
 #Variables---------------------------------
 DIR_ROOT <- file.path(getwd())
 DIR_DATA <- file.path(DIR_ROOT, "./Circulation/objects/individual/figures/")
+DIR_RES <-  file.path(DIR_ROOT, "./Circulation/results/Adrian.genes/silvia.december/segm/")
 
 #Data--------------------------------------
 samples <- dir(path = DIR_DATA)
@@ -28,9 +45,10 @@ samples <- dir(path = DIR_DATA)
 # Add area info in each sample
 lista <- c(samples)
 names(lista) <- samples
+lista <- lista[-1]
 prueba <-c()
 
-for (i in lista[-1]){
+for (i in lista){
   a <- lista[i]
   sam <- readRDS(paste0(DIR_DATA, i,"/", i,".rds"))
   ## add area data
@@ -55,6 +73,7 @@ dpi3@meta.data[["orig.ident"]] <- "dpi3"
 dpi5_female@meta.data[["orig.ident"]] <- "dpi5_female"
 dpi5_male@meta.data[["orig.ident"]] <- "dpi5_male"
 
+
 ##Merge them
 combined <- merge(control, y = c(dpi3,dpi5_female,dpi5_male), 
                   add.cell.ids = c("control", "dpi3", "dpi5_female", "dpi5_male"), project = "infart")
@@ -78,41 +97,193 @@ for (i in 1:length(objects)){
   ## Check intersect genes B and object
   v.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], verde))
   m.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], morado))
+  fb.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], fb.g))
+  nine.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], nine.g))
+  b.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], B))
+  da.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], DA))
+  db.genes <- list(intersect(a@assays[["SCT"]]@data@Dimnames[[1]], DB))
+  
   
   ## Add UCellScore
   a <- AddModuleScore_UCell(a, features = v.genes, name = "_verde")
   a <- AddModuleScore_UCell(a, features = m.genes, name = "_morado")
+  a <- AddModuleScore_UCell(a, features = m.genes, name = "_fb")
+  a <- AddModuleScore_UCell(a, features = nine.genes, name = "_nine")
+  a <- AddModuleScore_UCell(a, features = b.genes, name = "_b")
+  a <- AddModuleScore_UCell(a, features = da.genes, name = "_da")
+  a <- AddModuleScore_UCell(a, features = db.genes, name = "_db")
   
-  ## Calculate ratio
-  verde <- as.vector(a$signature_1_verde)
-  morado <- as.vector(a$signature_1_morado)
-  ratio <- log10(morado/verde)
+  
+  ## Calculate ratio verde/morado
+  v <- as.vector(a$signature_1_verde)
+  m <- as.vector(a$signature_1_morado)
+  ratio <- log10(m/v)
   is.na(ratio) <-sapply(ratio, is.infinite)
   ratio[is.na(ratio)] = 0
   a$ratio <- ratio
   
-  ## Save XXXXXXXX
-  saveRDS(a,file = paste0(DIR_DATA, names(objects[i]),"/enrich/", names(objects[i]),".enrich.rds"))
+  ## Calculate ratio da/db
+  da <- as.vector(a$signature_1_da)
+  db <- as.vector(a$signature_1_db)
+  ratio.dadb <- log10(da/db)
+  is.na(ratio.dadb) <-sapply(ratio.dadb, is.infinite)
+  ratio.dadb[is.na(ratio.dadb)] = 0
+  a$ratio.dadb <- ratio.dadb
+  
+  ## Regress out FB values
+  meta <- a@meta.data
+  lm <- lm(meta$signature_1_verde ~ meta$signature_1_fb, data =meta)
+  residuals <- lm$residuals
+  a@meta.data[["residuals_verde"]] <- residuals
+  
+  lm <- lm(meta$signature_1_morado ~ meta$signature_1_fb, data =meta)
+  residuals <- lm$residuals
+  a@meta.data[["residuals_morado"]] <- residuals
+  
+  ## Save
+  saveRDS(a,file = paste0(DIR_DATA, names(objects[i]),"/enrich/", names(objects[i]),".enrich_2.rds"))
   enrich[[length(enrich) + 1]] <- a
   
 }
 names(enrich) <- c("control", "dpi3","dpi5_female","dpi5_male")
 
+#####################seg.PLOTS#####################
+## Create a custom color scale
+nombres <- c("IZ","BZ_1","BZ_2","BZ_3","BZ_2_3","BZ_4","RZ")
+colors <- c("coral4", "skyblue", "red", "darkgreen", "yellow","khaki2", "darkolivegreen", "gray89")
+names(colors)  <- nombres
+
+## Objects
+new <- enrich[-1]
+
+for (i in 1:length(new)){
+  ## Object
+  a <- new[[i]]
+  
+  ## Order area as factor
+  a@meta.data[["area"]] <- factor(a@meta.data[["area"]],
+                                          levels = c("IZ","BZ_1","BZ_2","BZ_3","BZ_2_3","BZ_4","RZ"),ordered = TRUE)
+ 
+  ## boxplot
+  # verde
+  pdf(file.path(paste0(DIR_RES, names(new[i]),"/", names(new[i]), "boxplot_verde.pdf")))
+  print(a@meta.data%>% 
+    ggplot(aes(x=signature_1_verde, y= area, fill=area)) + 
+    geom_boxplot() +  
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    #xlim(-0.5, 0.5) +
+    theme(plot.title = element_text(hjust=0.5, face="bold")))
+  dev.off() 
+  pdf(file.path(paste0(DIR_RES, names(new[i]),"/", names(new[i]), "boxplot_verde_residuals.pdf")))
+  print(a@meta.data%>% 
+          ggplot(aes(x=residuals_verde, y= area, fill=area)) + 
+          geom_boxplot() +  
+          theme_classic() +
+          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+          #xlim(-0.5, 0.5) +
+          theme(plot.title = element_text(hjust=0.5, face="bold")))
+  dev.off() 
+  
+  # morado
+  pdf(file.path(paste0(DIR_RES, names(new[i]),"/", names(new[i]), "boxplot_morado.pdf")))
+  print(a@meta.data%>% 
+    ggplot(aes(x=signature_1_morado, y= area, fill=area)) + 
+    geom_boxplot() +  
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    #xlim(-0.5, 0.5) +
+    theme(plot.title = element_text(hjust=0.5, face="bold")))
+  dev.off()
+  pdf(file.path(paste0(DIR_RES, names(new[i]),"/", names(new[i]), "boxplot_morado_residuals.pdf")))
+  print(a@meta.data%>% 
+          ggplot(aes(x=residuals_morado, y= area, fill=area)) + 
+          geom_boxplot() +  
+          theme_classic() +
+          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+          #xlim(-0.5, 0.5) +
+          theme(plot.title = element_text(hjust=0.5, face="bold")))
+  dev.off() 
+  
+  pdf(file.path(paste0(DIR_RES, names(new[i]),"/", names(new[i]), "boxplot_ratio.pdf")))
+  print(a@meta.data%>% 
+    ggplot(aes(x=ratio, y= area, fill=area)) + 
+    geom_boxplot() +  
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
+    #xlim(-0.5, 0.5) +
+    theme(plot.title = element_text(hjust=0.5, face="bold")))
+  dev.off()
+  
+  ## spatial plots
+  library(BuenColors)
+  color <- jdb_palette("brewer_spectra")
+  # verde
+  b <- c(min(a@meta.data[["signature_1_verde"]]), max(a@meta.data[["signature_1_verde"]]))
+  p1 <- SpatialFeaturePlot(a, features = c("signature_1_verde"),  combine = FALSE, ncol = 1)
+  fix.p1 <- scale_fill_gradientn(colours=color,
+                                 breaks=b,
+                                 labels=c("Min","Max"),
+                                 limits = b)
+  p2 <- lapply(p1, function (x) x + fix.p1)
+  
+  pdf(paste0("./Circulation/results/Adrian.genes/silvia.december/Ucell/geneset/verde/",names(new[i]), "/", paste(names(new[i]), "_enrich_verde.pdf",sep="")))
+  print(CombinePlots(p2))
+  dev.off()
+  
+  b <- c(min(a@meta.data[["residuals_verde"]]), max(a@meta.data[["residuals_verde"]]))
+  p1 <- SpatialFeaturePlot(a, features = c("residuals_verde"),  combine = FALSE, ncol = 1)
+  fix.p1 <- scale_fill_gradientn(colours=color,
+                                 breaks=b,
+                                 labels=c("Min","Max"),
+                                 limits = b)
+  p2 <- lapply(p1, function (x) x + fix.p1)
+  
+  pdf(paste0("./Circulation/results/Adrian.genes/silvia.december/Ucell/geneset/verde/",names(new[i]), "/", paste(names(new[i]), "enrich_verde_residuals.pdf",sep="")))
+  print(CombinePlots(p2))
+  dev.off()
+
+  # morado
+  b <- c(min(a@meta.data[["signature_1_morado"]]), max(a@meta.data[["signature_1_morado"]]))
+  p1 <- SpatialFeaturePlot(a, features = c("signature_1_morado"),  combine = FALSE, ncol = 1)
+  fix.p1 <- scale_fill_gradientn(colours=color,
+                                 breaks=b,
+                                 labels=c("Min","Max"),
+                                 limits = b)
+  p2 <- lapply(p1, function (x) x + fix.p1)
+  
+  pdf(paste0("./Circulation/results/Adrian.genes/silvia.december/Ucell/geneset/morado/",names(new[i]), "/", paste(names(new[i]), "enrich_morado.pdf",sep="")))
+  print(CombinePlots(p2))
+  dev.off()
+  
+  b <- c(min(a@meta.data[["residuals_morado"]]), max(a@meta.data[["residuals_morado"]]))
+  p1 <- SpatialFeaturePlot(a, features = c("residuals_morado"),  combine = FALSE, ncol = 1)
+  fix.p1 <- scale_fill_gradientn(colours=color,
+                                 breaks=b,
+                                 labels=c("Min","Max"),
+                                 limits = b)
+  p2 <- lapply(p1, function (x) x + fix.p1)
+  
+  pdf(paste0("./Circulation/results/Adrian.genes/silvia.december/Ucell/geneset/morado/",names(new[i]), "/", paste(names(new[i]), "enrich_morado_residuals.pdf",sep="")))
+  print(CombinePlots(p2))
+  dev.off()
+  
+  # ratio
+  b <- c(min(a@meta.data[["ratio"]]), max(a@meta.data[["ratio"]]))
+  p1 <- SpatialFeaturePlot(a, features = c("ratio"),  combine = FALSE, ncol = 1)
+  fix.p1 <- scale_fill_gradientn(colours=color,
+                                 breaks=b,
+                                 labels=c("Min","Max"),
+                                 limits = b)
+  p2 <- lapply(p1, function (x) x + fix.p1)
+  
+  pdf(paste0("./Circulation/results/Adrian.genes/silvia.december/Ucell/geneset/ratio/",names(new[i]), "/", paste(names(new[i]), "ratio.pdf",sep="")))
+  print(CombinePlots(p2))
+  dev.off()
+}
 
 
-##########################################################
-######spatial
-heart <- readRDS("./Circulation/objects/integrated/integrated.rds")
-DefaultAssay(heart) <- "SCT"
-
-###### Check intersect genes B and object
-v.genes <- intersect(heart@assays[["SCT"]]@data@Dimnames[[1]], verde)
-v.list <- as.list(v.genes)
-names(v.list) <- as.list(v.genes)
-m.genes <- intersect(heart@assays[["SCT"]]@data@Dimnames[[1]], morado)
-m.list <- as.list(m.genes)
-names(m.list) <- as.list(m.genes)
-
+###################################################
 ##### Check individually
 
 
@@ -125,25 +296,6 @@ for (i in m.list){
 }
 
 
-##### Add moduleScore
-DefaultAssay(heart) <- "Spatial"
-v.genes <- list(intersect(heart@assays[["SCT"]]@data@Dimnames[[1]], verde))
-heart_module <- AddModuleScore(heart, features = v.genes, name = c("verde_"))
-m.genes <- list(intersect(heart@assays[["SCT"]]@data@Dimnames[[1]], morado))
-heart_module <- AddModuleScore(heart_module, features = m.genes, name = c("morado_"))
-
-##### Add UCellScore
-
-heart_Uscore <- AddModuleScore_UCell(heart, features = v.genes, name = "_verde")
-heart_Uscore <- AddModuleScore_UCell(heart_Uscore, features = m.genes, name = "_morado")
-
-##### Calculate relation d1/d2
-verde <- as.vector(heart_Uscore$signature_1_verde)
-morado <- as.vector(heart_Uscore$signature_1_morado)
-ratio <- log10(morado/verde)
-is.na(ratio) <-sapply(ratio, is.infinite)
-ratio[is.na(ratio)] = 0
-heart_Uscore$ratio <- ratio
 
 #####
 
